b9f8e794fdf3a609c819c3eb559282d0d0ad98d3
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2017-04-03 11:36:26 +0530

    Reactjs setup

---
+++ require.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Page = require('./app/pages/page.js');
+
+ReactDOM.render(<Page />, document.getElementById('container'));
